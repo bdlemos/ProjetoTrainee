3dc52844e7519599255637388289c4584e595c5e
Kauan Pereira <114433504+kauantpereira@users.noreply.github.com>
2023-10-14 11:50:46 -0300

    correção bug das musicas repetidas

--- index.ts
+++ index.ts
@@ -13,22 +13,29 @@ async function main(){
     } as User);
 
     //TESTE DAS FUNÇÕES DE MÚSICA
-    /*
-    await addSong({
+    const song1 = await addSong({
         name: 'Fragments of Time',
         genre: 'Eletônica',
         album: 'Randon Acess Memories',
         artistId: 1,
     } as Song);
 
-    await addSong({
+    const song2 = await addSong({
         name: 'Por Enquanto',
         genre: 'MPB',
         album: 'Cássia Eller',
         artistId: 2,
     } as Song);
+
+    const song3 = await addSong({
+        name: 'Baby',
+        genre: 'Pop',
+        album: 'Believe',
+        artistId: 3,
+    } as Song);
     
-    await updateSong({ //erro
+    /*
+    const teste = await updateSong({ //erro
         id: 1,
         name: 'Fragments of Time',
     } as Song);
