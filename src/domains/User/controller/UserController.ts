bdec659266bb04ad317571c4c663cdb91dd35c2a
Macellesm <107588350+Macellesm@users.noreply.github.com>
2023-10-14 16:15:30 -0300

    Update UserController.ts

--- src/domains/User/controller/UserController.ts
+++ src/domains/User/controller/UserController.ts
@@ -1,11 +1,38 @@
 import { User } from "@prisma/client";
 import UserService from "../service/UserService";
 
-export async function addUser(data: User){
-    try{
-        await UserService.create(data); 
-        console.log("Sucesso!");
-    } catch(error){
-    console.log("Erro ao adicionar usuário."); //fazer visualização do erro depois
-    }
+export async function addUser(data: User) {
+  try {
+    await UserService.create(data);
+    console.log("Sucesso ao adicionar usuário!");
+  } catch (error) {
+    console.log("Erro ao adicionar usuário:", error);
+  }
+}
+
+export async function updateUser(data: User) {
+  try {
+    await UserService.update(data);
+    console.log("Sucesso ao atualizar usuário!");
+  } catch (error) {
+    console.log("Erro ao atualizar usuário:", error);
+  }
+}
+
+export async function listUsers() {
+  try {
+    const users = await UserService.list();
+    console.log("Lista de usuários:", users);
+  } catch (error) {
+    console.log("Erro ao listar usuários:", error);
+  }
+}
+
+export async function deleteUser(userId: number) {
+  try {
+    await UserService.delete(userId);
+    console.log("Usuário excluído com sucesso!");
+  } catch (error) {
+    console.log("Erro ao excluir usuário:", error);
+  }
 }
