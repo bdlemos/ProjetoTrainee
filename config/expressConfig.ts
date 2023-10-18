d7474095c084601c7b8a97504b6af6128cbd6cde
Pedro Paulo Oliveira Salomão <ppoliveirasalomao@gmail.com>
2023-10-18 14:14:12 -0300

    .env adicionado

--- config/expressConfig.ts
+++ config/expressConfig.ts
@@ -8,14 +8,14 @@ dotenv.config();
 export const app: Express = express();
 
 const options : CorsOptions = {
-    credentials: true,
-    origin: process.env.APP_URL
+	credentials: true,
+	origin: process.env.APP_URL
 };
 
 app.use(cors(options));
 app.use(express.json());
 app.use(express.urlencoded({
-    extended: true
+	extended: true
 }));
 app.use('/api/users', UserRouter);
 
--- src/domains/User/controller/UserController.ts
+++ src/domains/User/controller/UserController.ts
@@ -1,83 +1,85 @@
-import { User } from "@prisma/client";
-import UserService from "../service/UserService";
-import { Router, Request, Response, NextFunction } from "express";
+import { User } from '@prisma/client';
+import UserService from '../service/UserService';
+import { Router, Request, Response, NextFunction } from 'express';
 
 const router = Router();
 
 router.get('/', async(req:Request, res:Response, next:NextFunction) => {
-  try {
-      const users = await UserService.getAll();
-      res.json(users);
-  } catch (error) {
-      next(error);
-    }
+	try {
+		const users = await UserService.getAll();
+		res.json(users);
+	} catch (error) {
+		next(error);
+	}
 })
 
 router.get('/:email', async(req:Request, res:Response, next:NextFunction) => {
-  try {
-      const users = await UserService.getByEmail(req.params.email);
-      res.json(users);
-  } catch (error) {
-      next(error);
-    }
+	try {
+		const users = await UserService.getByEmail(req.params.email);
+		res.json(users);
+	} catch (error) {
+		next(error);
+	}
 })
 
 router.post('/create', async(req:Request, res:Response, next:NextFunction) => {
-  try {
-      await UserService.create(req.body);
-      res.json('Usuário criado com sucesso!');
-  } catch (error) {
-      next(error);
-    }
+	try {
+		await UserService.create(req.body);
+		res.json('Usuário criado com sucesso!');
+	} catch (error) {
+		next(error);
+	}
 })
 
 export async function getAllUser() {
-  try {
-    const users = await UserService.getAll();
-    console.log("Lista de usuários:", users);
-  } catch (error) {
-    console.log("Erro ao listar usuários:", error);
-  }
+	try {
+		const users = await UserService.getAll();
+		console.log('Lista de usuários:', users);
+	} catch (error) {
+		console.log('Erro ao listar usuários:', error);
+	}
 }
 
 
 export async function addUser(data: User) {
-  try {
-    await UserService.create(data);
-    console.log("Sucesso ao adicionar usuário!");
-  } catch (error) {
-    console.log("Erro ao adicionar usuário:", error);
-  }
+	try {
+		await UserService.create(data);
+		console.log('Sucesso ao adicionar usuário!');
+	} catch (error) {
+		console.log('Erro ao adicionar usuário:', error);
+	}
 }
 
 export async function updateUser(data: User) {
-  try {
-    await UserService.update(data);
-    console.log("Sucesso ao atualizar usuário!");
-  } catch (error) {
-    console.log("Erro ao atualizar usuário:", error);
-  }
+	try {
+		await UserService.update(data);
+		console.log('Sucesso ao atualizar usuário!');
+	} catch (error) {
+		console.log('Erro ao atualizar usuário:', error);
+	}
 }
 
 
 export async function getByUserEmail(email: string) {
-  try {
-    const user = await UserService.getByEmail(email);
-    if (user) {
-      console.log("Usuário encontrado:", user);
-    } else {
-      console.log("Nenhum usuário encontrado com o e-mail fornecido.");
-    }
-  } catch (error) {
-    console.log("Erro ao buscar usuário por e-mail:", error);
-  }
+	try {
+		const user = await UserService.getByEmail(email);
+		if (user) {
+			console.log('Usuário encontrado:', user);
+		} else {
+			console.log('Nenhum usuário encontrado com o e-mail fornecido.');
+		}
+	} catch (error) {
+		console.log('Erro ao buscar usuário por e-mail:', error);
+	}
 }
 
 export async function deleteUser(userId: number) {
-  try {
-    await UserService.delete(userId);
-    console.log("Usuário excluído com sucesso!");
-  } catch (error) {
-    console.log("Erro ao excluir usuário:", error);
-  }
+	try {
+		await UserService.delete(userId);
+		console.log('Usuário excluído com sucesso!');
+	} catch (error) {
+		console.log('Erro ao excluir usuário:', error);
+	}
 }
+
+export default router;
