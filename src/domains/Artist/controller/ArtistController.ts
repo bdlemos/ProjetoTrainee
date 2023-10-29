f3988052e6e252ad0331f2cd189efba78dc4b2fa
bdlemos <bdutralemos@gmail.com>
2023-10-29 19:36:44 -0300

    feat: implantação dos status nas rotas de artistas

--- src/domains/Artist/controller/ArtistController.ts
+++ src/domains/Artist/controller/ArtistController.ts
@@ -1,7 +1,8 @@
 import { Artist } from '@prisma/client';
 import ArtistService from '../service/ArtistService';
 import { Router, Request, Response, NextFunction } from 'express';
-import { create } from 'domain';
+import { checkRole } from '../../../middlewares/checkRole';
+import statusCodes from '../../../../utils/constants/statusCode';
 
 const router = Router();
 
@@ -9,8 +10,9 @@ const router = Router();
 router.get('/', async(req:Request, res:Response, next:NextFunction) => {
 	try{
 		const Artists = await ArtistService.getAll(); 
-		res.json(Artists);
+		res.status(statusCodes.SUCCESS).json(Artists);
 	} catch(error){ 
+		res.status(statusCodes.NOT_FOUND);
 		next(error);
 	}
 })
@@ -18,13 +20,16 @@ router.get('/', async(req:Request, res:Response, next:NextFunction) => {
 router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		const artist = await ArtistService.getByID(parseInt(req.params.id)); 
-		res.json(artist);
+		res.status(statusCodes.SUCCESS).json(artist);
 	} catch (error) { 
+		res.status(statusCodes.NOT_FOUND);
 		next(error);
 	}
 })
 
-router.post('/create', async (req: Request, res: Response, next: NextFunction) => {
+router.post('/create',
+	checkRole("admin"),
+	async (req: Request, res: Response, next: NextFunction) => {
 	try{
 		const createData = {
 			id: +req.params.id,
@@ -33,7 +38,7 @@ router.post('/create', async (req: Request, res: Response, next: NextFunction) =
 			photo: req.body.photo
 		} as Artist;
 		await ArtistService.create(createData); 
-		res.json('Artista adicionado com sucesso!');
+		res.status(statusCodes.CREATED).json('Artista adicionado com sucesso!');
 	} catch(error){
 		next(error);
 	}
@@ -42,7 +47,9 @@ router.post('/create', async (req: Request, res: Response, next: NextFunction) =
 
 
 
-router.put('/update/:id', async (req: Request, res: Response, next: NextFunction) => {
+router.put('/update/:id',
+	checkRole("admin"),
+	async (req: Request, res: Response, next: NextFunction) => {
 	try{
 		const updateData = {
 			id: +req.params.id,
@@ -51,16 +58,18 @@ router.put('/update/:id', async (req: Request, res: Response, next: NextFunction
 			photo: req.body.photo
 		} as Artist;
 		await ArtistService.update(updateData); 
-		res.json('Artista atualizado com sucesso!');
+		res.status(statusCodes.SUCCESS).json('Artista atualizado com sucesso!');
 	} catch(error){ 
 		next(error);
 	}
 })
 
-router.delete('/remove/:id', async (req: Request, res: Response, next: NextFunction) =>{
+router.delete('/remove/:id',
+	//checkRole("admin"), 
+	async (req: Request, res: Response, next: NextFunction) =>{
 	try{
 		await ArtistService.delete(+req.params.id); 
-		res.json('Artista removido com sucesso!');
+		res.status(statusCodes.SUCCESS).json('Artista removido com sucesso!');
 	} catch(error){ 
 		next(error);
 	}
